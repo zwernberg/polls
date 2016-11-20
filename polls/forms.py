a9a59582632d7f71d280806ac3ee1be3271c2528
Zach Wernberg <zawerny@gmail.com>
2016-11-20 14:09:13 -0600

    have form use bootstrap inputs

--- polls/forms.py
+++ polls/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from django.forms.models import inlineformset_factory
 
@@ -7,11 +8,17 @@ class QuestionForm(ModelForm):
     class Meta:
         model = Question
         fields = ['question_text']
+        widgets = {
+            'question_text': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 class ChoiceForm(ModelForm):
     class Meta:
         model = Choice
         fields = ['choice_text']
+        widgets = {
+            'choice_text': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 ChoiceFormSet = inlineformset_factory(Question,Choice,form=ChoiceForm, fields=('choice_text',), extra=0 ,min_num=1, max_num=5 )
 
