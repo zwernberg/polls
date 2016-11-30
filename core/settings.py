66a2c39c26ad975cd9014648755999e3dc2d5770
Zach Wernberg <zawerny@gmail.com>
2016-11-29 21:46:44 -0600

    add all origins for cors

--- core/settings.py
+++ core/settings.py
@@ -39,11 +39,8 @@ else:
     ALLOWED_HOSTS =['*']
     
 # Cors
-# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_WHITELIST = (
-    '192.241.159.51'
-)
 
 CORS_ALLOW_HEADERS = (
     'x-requested-with',
